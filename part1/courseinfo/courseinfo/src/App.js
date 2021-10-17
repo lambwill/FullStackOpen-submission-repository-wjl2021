e028cb07b14b993286f68b6fc637f88b271cc83e
Will Lamb <lambwill@gmail.com>
2021-10-17 13:16:24 +0100

    Exercise 1.2

--- part1/courseinfo/courseinfo/src/App.js
+++ part1/courseinfo/courseinfo/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const course =' Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const Header = (props) => (
+      <h1>{props.course}</h1>
+  )
+
+  const Part = (props) => (
+    <p>{props.part} {props.exercises}</p>
+  )
+
+  const Content = () => (
+    <div>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </div>
-  );
+  )
+
+  const Footer = () => (
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  )
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content />
+      <Footer />
+    </div>
+
+  )
 }
 
 export default App;
