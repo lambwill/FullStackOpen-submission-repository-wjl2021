c1e96bb0d2667e28e83823e698bd29d5d0360bd6
Will Lamb <lambwill@gmail.com>
2021-10-19 09:07:38 +0100

    Exercise 1.13 (anecdotes step 2)

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,16 @@
 import React, { useState } from 'react'
 
+function getRandomInt(min, max) {
+  // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+const Button = ({ text, clickHandler}) => (
+  <button onClick={clickHandler}>{text}</button>
+)
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -10,12 +21,27 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
-   
   const [selected, setSelected] = useState(0)
+  const initialPoints = new Array(anecdotes.length).fill(0)
+  const [points, setPoints] = useState(initialPoints)
+
+  const nextAnecdote = () => {
+    const randomInt = getRandomInt(0,anecdotes.length - 1)
+    setSelected(randomInt)
+  }
+
+  const vote = () => {
+    const pointsCopy = [...points]
+    pointsCopy[selected] += 1
+    setPoints(pointsCopy)
+  }
 
   return (
     <div>
-      {anecdotes[selected]}
+      <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <Button text="vote" clickHandler={vote} />
+      <Button text="next anecdote" clickHandler={nextAnecdote} />
     </div>
   )
 
