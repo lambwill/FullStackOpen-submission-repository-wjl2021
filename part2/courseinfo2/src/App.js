897ee57d59be2ea08aaa401fe3650b9a0fbf93e3
Will Lamb <lambwill@gmail.com>
2021-10-23 12:05:20 +0100

    2.1: Course information step6

--- part2/courseinfo2/src/App.js
+++ part2/courseinfo2/src/App.js
@@ -21,40 +21,47 @@ const Part = (props) => {
   )
 }
 
-const Content = ({ course }) => {
+const Course = ({ course }) => {
   return (
     <div>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
+      <Header course={course} />
+      {course.parts.map(part => 
+        <Part part={part} key={part.id}/>)}
     </div>
   )
 }
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Other',
+        exercises: 12,
+        id: 4
       }
     ]
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Course course={course} />
     </div>
   )
 }
