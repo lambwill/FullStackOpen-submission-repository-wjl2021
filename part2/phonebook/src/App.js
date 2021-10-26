a6cce911892aba19b8dd3226ab6574b491b3ebf4
Will Lamb <lambwill@gmail.com>
2021-10-26 08:43:45 +0100

    2.7: The Phonebook Step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,39 @@
 import React, { useState } from 'react'
 
+const Numbers = ({ numbers }) => {
+  console.log("numbers =", numbers)
+  return (
+    <div>
+      {numbers.map( number => <Number number={number} key={number.id}/>)}
+    </div>
+  )
+  
+}
+
+const Number = ( {number} ) => 
+{console.log("number =", number)
+  return (
+    <p>{number.name}</p>
+  )
+}
+
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', id: 0 }
   ]) 
   const [ newName, setNewName ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
-    setPersons(persons.concat({ name: newName}))
+    const names = persons.map( person => person.name)
+    console.log(names);
+    const pos = names.indexOf(newName)
+    console.log(pos);
+    if (pos === -1) {
+      setPersons(persons.concat({ name: newName, id:persons.length}))
+    } else {
+      window.alert(newName + " is already added to phonebook")
+    }
   }
 
   const handleNameChange = (event) => {
@@ -30,6 +55,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
+      <Numbers numbers={persons} />
       <div>debug: {newName}</div>
     </div>
   )
